389c8142fb118173ae222cd7d57dc27ae1a894e4
ChengYuDa <xeriof12358@gmail.com>
2016-10-26 15:02:47 +0800

    點Button跳服務視窗

--- app/src/main/java/com/javatechig/listapps/Detector.java
+++ app/src/main/java/com/javatechig/listapps/Detector.java
@@ -1,54 +1,54 @@
-package com.javatechig.listapps;
-
-import android.app.Activity;
-import android.app.Application;
-import android.os.Bundle;
-import android.util.Log;
-
-
-/**
- * Created by ChengYuDa on 2016/10/11.
- */
-
-public class Detector implements Application.ActivityLifecycleCallbacks {
-
-    private int run = 0;
-    private int resume = 0;
-    private int create = 0;
-
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        System.out.println("Some App create now "+(++create));
-    }
-
-    @Override
-    public void onActivityStarted(Activity activity) {
-        System.out.println("Some App running now "+(++run));
-        Log.e("","onActivityStarted:" + activity.getLocalClassName());
-    }
-
-    @Override
-    public void onActivityResumed(Activity activity) {
-        System.out.println("Some App resuming now "+(++resume));
-    }
-
-    @Override
-    public void onActivityPaused(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-
-    }
-}
+//package com.javatechig.listapps;
+//
+//import android.app.Activity;
+//import android.app.Application;
+//import android.os.Bundle;
+//import android.util.Log;
+//
+//
+///**
+// * Created by ChengYuDa on 2016/10/11.
+// */
+//
+//public class Detector implements Application.ActivityLifecycleCallbacks {
+//
+//    private int run = 0;
+//    private int resume = 0;
+//    private int create = 0;
+//
+//    @Override
+//    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+//        System.out.println("Some App create now "+(++create));
+//    }
+//
+//    @Override
+//    public void onActivityStarted(Activity activity) {
+//        System.out.println("Some App running now "+(++run));
+//        Log.e("","onActivityStarted:" + activity.getLocalClassName());
+//    }
+//
+//    @Override
+//    public void onActivityResumed(Activity activity) {
+//        System.out.println("Some App resuming now "+(++resume));
+//    }
+//
+//    @Override
+//    public void onActivityPaused(Activity activity) {
+//
+//    }
+//
+//    @Override
+//    public void onActivityStopped(Activity activity) {
+//
+//    }
+//
+//    @Override
+//    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+//
+//    }
+//
+//    @Override
+//    public void onActivityDestroyed(Activity activity) {
+//
+//    }
+//}
--- app/src/main/java/com/javatechig/listapps/sudoactivity.java
+++ app/src/main/java/com/javatechig/listapps/sudoactivity.java
@@ -1,34 +1,34 @@
-package com.javatechig.listapps;
-
-import android.content.Context;
-import android.app.ActivityManager;
-import android.app.ActivityManager.*;
-import android.widget.Toast;
-
-import java.util.List;
-
-import static android.content.Context.ACTIVITY_SERVICE;
-
-/**
- * Created by ChengYuDa on 2016/10/11.
- */
-
-public class sudoactivity {
-    Context myContext;
-    public sudoactivity(Context context){
-        this.myContext = myContext;
-    }
-    public void Doit(){
-        ActivityManager activityManager = (ActivityManager) myContext.getSystemService( ACTIVITY_SERVICE );
-        List<RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
-        for(int i = 0; i < procInfos.size(); i++)
-        {
-            System.out.println(procInfos.get(i).processName);
-            if(procInfos.get(i).processName.equals("com.google.android.youtube"))
-            {
-                System.out.println("Youtuber is running");
-                Toast.makeText(myContext.getApplicationContext(), "Youtuber is running", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-}
+//package com.javatechig.listapps;
+//
+//import android.content.Context;
+//import android.app.ActivityManager;
+//import android.app.ActivityManager.*;
+//import android.widget.Toast;
+//
+//import java.util.List;
+//
+//import static android.content.Context.ACTIVITY_SERVICE;
+//
+///**
+// * Created by ChengYuDa on 2016/10/11.
+// */
+//
+//public class sudoactivity {
+//    Context myContext;
+//    public sudoactivity(Context context){
+//        this.myContext = myContext;
+//    }
+//    public void Doit(){
+//        ActivityManager activityManager = (ActivityManager) myContext.getSystemService( ACTIVITY_SERVICE );
+//        List<RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+//        for(int i = 0; i < procInfos.size(); i++)
+//        {
+//            System.out.println(procInfos.get(i).processName);
+//            if(procInfos.get(i).processName.equals("com.google.android.youtube"))
+//            {
+//                System.out.println("Youtuber is running");
+//                Toast.makeText(myContext.getApplicationContext(), "Youtuber is running", Toast.LENGTH_LONG).show();
+//            }
+//        }
+//    }
+//}
