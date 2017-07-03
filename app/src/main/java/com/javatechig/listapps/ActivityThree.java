ae2be7eb04e29365dbb58afb3c98a14dfac52d26
HsinWei <scott02308@gmail.com>
2017-07-03 16:01:00 +0800

    add code on  ActivityThree.                   1. Get signal of wifi                   2. UI

--- app/src/main/java/com/javatechig/listapps/ActivityThree.java
+++ app/src/main/java/com/javatechig/listapps/ActivityThree.java
@@ -1,14 +1,27 @@
 package com.javatechig.listapps;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by User on 4/15/2017.
@@ -16,13 +29,27 @@ import android.widget.TextView;
 
 public class ActivityThree extends AppCompatActivity {
 
+    private TextView rssiTextView;
+    private Button startRssi;
+    private ProgressBar pb;
+
+    private Handler mThreadHandler;
+    private HandlerThread mThread;
+
+    //首先取得Wi-Fi服務控制Manager
+    private WifiManager mWifiManager;
+    private List<ScanResult> scanResult;
+    private List<WifiConfiguration> wifiConfigurations;
+    private List<WifiConfiguration> configs;
+    private WifiInfo info;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
 
         TextView title = (TextView) findViewById(R.id.activityTitle3);
-        title.setText("This is ActivityThree");
+        title.setText("");
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
@@ -61,6 +88,55 @@ public class ActivityThree extends AppCompatActivity {
 
 
                 return false;
+            }
+        });
+
+
+        rssiTextView = (TextView)findViewById(R.id.rssiShow);
+        startRssi = (Button)findViewById(R.id.buttonRssi);
+        pb = (ProgressBar)findViewById(R.id.progressBarRssi);
+        pb.setProgress(0);
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        mWifiManager.setWifiEnabled(true);
+        mWifiManager.startScan();
+
+        mThread = new HandlerThread("getRssi");
+        mThread.start();
+
+        mThreadHandler=new Handler(){
+            int i = 0;
+            @Override
+            public void handleMessage(Message msg) {
+                switch(msg.what){
+                    case 1:
+                        rssiTextView.setText(Integer.toString(mWifiManager.getConnectionInfo().getRssi()));
+                        pb.setProgress(mWifiManager.getConnectionInfo().getRssi()+100);
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        startRssi.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        while(true){
+                            try{
+                                Message msg = new Message();
+                                msg.what = 1;
+                                mThreadHandler.sendMessage(msg);
+                                Thread.sleep(10);
+                            }
+                            catch(Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
             }
         });
     }
