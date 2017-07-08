e3d8e056e7d9f3db8087d0609e1ce1a604f05a1a
Richard Yi-Chia Tsai <richard.yctsai@gmail.com>
2017-07-09 01:43:11 +0800

    Upload the congestion and cellid to server

--- app/src/main/java/com/javatechig/listapps/ActivityThree.java
+++ app/src/main/java/com/javatechig/listapps/ActivityThree.java
@@ -2,6 +2,7 @@ package com.javatechig.listapps;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.TrafficStats;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
@@ -10,10 +11,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,16 +24,48 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+import java.util.UUID;
+
+import android.content.Context;
+import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager;
+import android.telephony.cdma.CdmaCellLocation;
+import android.telephony.gsm.GsmCellLocation;
 
 /**
  * Created by User on 4/15/2017.
  */
 
 public class ActivityThree extends AppCompatActivity {
+
+    private String HOST = "192.168.43.176";
+    private Handler mHandler = new Handler();
+    String congestionSentToServer = "";
+    private int CongestionCount = 0;
+    private JSONObject jsonObjCongestion = new JSONObject();
+    private JSONArray jsonArrCongestion = new JSONArray();
+
+    private TelephonyManager telephonyManager;
+    private GsmCellLocation cellLocation;
+    private int cid;
+    private int lac;
 
     private TextView rssiTextView;
     private TextView transfer_rate_View;
@@ -55,10 +90,26 @@ public class ActivityThree extends AppCompatActivity {
 
     static String busyornot = "";
 
+    private static String deviceId = "5341231";
+    public static String getdeviceId(){
+        return deviceId;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+
+        mHandler.postDelayed(mRunnable, 1000);
+
+        try {
+            deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        cellLocation = (GsmCellLocation) telephonyManager.getCellLocation();
 
         TextView title = (TextView) findViewById(R.id.activityTitle3);
         title.setText("");
@@ -186,6 +237,142 @@ public class ActivityThree extends AppCompatActivity {
             public void onClick(View view) {
                 transfer_rate_View.setText("上下載速度: "+sim_in.getText().toString()+"Mb/s");
                 MyFirebaseMessagingService.transfer_rate_bit = Double.parseDouble(sim_in.getText().toString());
+            }
+        });
+    }
+
+    private final Runnable mRunnable = new Runnable() {
+        public void run() {
+
+            System.out.println(getCurrentTime());
+
+            if (Integer.parseInt(getSec()) % 10 == 0) { //getMin().toString().equals("00") && getSec().toString().equals("00") getMin().toString().equals("00") && getMin().toString().equals("00") && Integer.parseInt(getSec()) % 10 == 0   getSec().toString().equals("00")     getMin().toString().equals("00") && getSec().toString().equals("00")
+                try {
+                    // Here we convert Java Object to JSON
+                    CongestionCount++;
+                    JSONObject congestionObj = new JSONObject();
+                    congestionObj.put("serial_id", CongestionCount);
+                    congestionObj.put("mac_id", deviceId);
+                    congestionObj.put("date", getDate());
+                    congestionObj.put("clock", getClock());
+                    congestionObj.put("congestion", busyornot);
+                    congestionObj.put("cell_id", cellLocation.getCid());
+                    congestionObj.put("lac", cellLocation.getLac());
+
+                    jsonArrCongestion.put(congestionObj);
+                    jsonObjCongestion.put("congestiondatabases", jsonArrCongestion);
+
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            if (Integer.parseInt(getSec()) % 10 == 0) {
+                    RequestParams paramsCongestion = new RequestParams();
+                    congestionSentToServer = jsonObjCongestion.toString();
+//                    congestionSentToServer = calculateCongestion();
+                    System.out.println(congestionSentToServer);
+
+                    // 送Congestion的通道,  要送的東西放在congestionSentToServer , 格式幫忙弄成json , 第一格放MAC ID ,  第二格當下時間 第三四格擁塞與否
+                    paramsCongestion.put("congestion", congestionSentToServer);
+                    passToServer(paramsCongestion, "CHT-congestion");
+            }
+
+            mHandler.postDelayed(mRunnable, 1000);
+        }
+    };
+
+//    private String calculateCongestion() {
+//        String serial_id = "";
+//        String mac_id = "";
+//        String enodeb_id = "";
+//        String date = "";
+//        String clock = "";
+//        String congestion = "";
+//        String cell_id = "";
+//        String lac = "";
+//
+//        try {
+//            JSONArray congestiondatabases = jsonObjCongestion.getJSONArray("congestiondatabases");
+//
+//            for (int i = 0; i < congestiondatabases.length(); i++) {
+//                JSONObject c = congestiondatabases.getJSONObject(i);
+//
+//                serial_id = c.getString("macid");
+//                mac_id = c.getString("id");
+//                enodeb_id = c.getString("enodeb_id");
+//                date = c.getString("date");
+//                clock = c.getString("clock");
+//                congestion = c.getString("congestion");
+//                cell_id = c.getString("cell_id");
+//                lac = c.getString("lac");
+//            }
+//
+//        } catch (JSONException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        return jsonObjCongestion.toString();
+//    }
+
+    public static String getCurrentTime() {
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => " + c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+
+        return formattedDate;
+    }
+
+    public static String getSec() {
+        Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat df = new SimpleDateFormat("ss");
+        String formattedDate = df.format(c.getTime());
+
+        return formattedDate;
+    }
+
+    public static String getDate() {
+        Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = df.format(c.getTime());
+
+        return formattedDate;
+    }
+
+    public static String getClock() {
+        Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+
+        return formattedDate;
+    }
+
+    //傳至Server
+    public void passToServer(RequestParams params, String tube){ //tube: CHT-flow 送流量   tube:CHT-feature 送feature
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://" + HOST + ":8080/CHTServer/hello/"+tube;
+        System.out.println(url);
+        client.get(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                CharSequence cs = new String(bytes);
+                Toast toast = Toast.makeText(getApplicationContext(), cs, Toast.LENGTH_SHORT);    //toast 會閃現    用textView來接
+                toast.show();
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Log.e("InvokeWS", Integer.toString(i));
+                if (bytes != null) {
+                    CharSequence cs = new String(bytes);
+                    Toast toast = Toast.makeText(getApplicationContext(), cs, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
